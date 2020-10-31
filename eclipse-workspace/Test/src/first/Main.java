c55c15c29ce8287076fd337bd1096841c7d13d70
JekaRozel <jeka.rozel@gmail.com>
2020-11-01 02:40:31 +0300

    a

--- eclipse-workspace/Test/src/first/Main.java
+++ eclipse-workspace/Test/src/first/Main.java
@@ -1,9 +1,21 @@
 package first;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lenth = scanner.nextInt();
+        int[] mas = new int[lenth];
+        mas[0] = 0;
+        for (int i = 1; i < lenth; i++) {
+            mas[i] = scanner.nextInt();
+            System.out.println(mas[i] + " ");
+        } 
+        mas[0] = scanner.nextInt();
+        for (int i : mas) {
+        	System.out.print(i + " | ");
+        }
+        // TODO Auto-generated method stub
 //		System.out.println("Hello!");
 //		String a = "abcdefghijklmnopqrstwxyz";
 //		String b = new String("abcdefghijklmnopqrstwxyz");
@@ -14,7 +26,7 @@ public class Main {
 //		System.out.println(a.indexOf('c'));
 //		System.out.println(a.substring(4,6));
 //		BeerSong.beerBottle();
-		Shuffle1.shuf();
+//		Shuffle1.shuf();
 //int a = 12;
 //System.out.println(a%100);
 //System.out.println((a%100)/10);	
