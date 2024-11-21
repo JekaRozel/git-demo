d5377b418020fbd16ee6cbe7259202995fea7416
JekaRozel <https://github.com/JekaRozel>
2024-11-22 02:47:03 +0300

    add App.java & AppTest.java

---
+++ eclipse-workspace/AMavenProject/artifactIdMavenProject/src/main/java/groupIdMavenProject/artifactIdMavenProject/App.java
@@ -0,0 +1,13 @@
+package groupIdMavenProject.artifactIdMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
